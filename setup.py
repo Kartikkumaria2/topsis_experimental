112a8e40acac3a2c6b6e6d4fb99b700f7e5d8cb3
kartikkumaria2 <kkumaria_be21@thapar.edu>
2024-01-28 01:29:34 +0530

    first

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='topsis_experimental',  # <--- Updated name
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scikit-learn',
+    ],
+    entry_points={
+    'console_scripts': [
+        'topsis_experimental = topsis_experimental.topsis_demo:main',  # <--- Updated name
+    ],
+},
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+)
