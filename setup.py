9ae6c4e8aad4ff73674f67cd4255e3f6a8e24664
kartikkumaria2 <kkumaria_be21@thapar.edu>
2024-01-28 01:56:45 +0530

    dew

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='topsis_experimental',  # <--- Updated name
-    version='0.3',
+    version='0.4',
     packages=find_packages(),
     install_requires=[
         'pandas',
@@ -10,10 +10,10 @@ setup(
         'scikit-learn',
     ],
     entry_points={
-    'console_scripts': [
-        'topsis_experimental = topsis_experimental.topsis_demo:main',  # <--- Updated name
-    ],
-},
+        'console_scripts': [
+            'topsis_experimental = topsis_experimental.__main__:main'
+        ],
+    },
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',
 )
